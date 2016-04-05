d45e08c21b66d590046e2a7ece3e312c8ba4c568
Nurdianto <nurdiantopntr@gmail.com>
2016-04-05 14:52:40 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+cd /
+mkdir -p /home/keyword/result
+cd /home/keyword
+touch /home/keyword/keyword.txt
+wget https://github.com/nurd1n/bing-image-keyword-spy/raw/secret/build.sh --no-check-certificate --directory-prefix=/home/keyword
+wget https://github.com/nurd1n/bing-image-keyword-spy/raw/secret/start1.sh --no-check-certificate --directory-prefix=/home/keyword
+wget https://github.com/nurd1n/bing-image-keyword-spy/raw/secret/start2.sh --no-check-certificate --directory-prefix=/home/keyword
+wget https://github.com/nurd1n/bing-image-keyword-spy/raw/secret/title.py --no-check-certificate --directory-prefix=/home/keyword
+chmod 755 *.sh
