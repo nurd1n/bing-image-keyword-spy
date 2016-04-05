46a31f7d2964f17dbe83149d5ca834328fa7981c
Nurdianto <nurdiantopntr@gmail.com>
2016-04-05 15:38:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ touch /home/keyword/keyword.txt
 wget https://github.com/nurd1n/bing-image-keyword-spy/raw/secret/build.sh --no-check-certificate --directory-prefix=/home/keyword
 wget https://github.com/nurd1n/bing-image-keyword-spy/raw/secret/start1.sh --no-check-certificate --directory-prefix=/home/keyword
 wget https://github.com/nurd1n/bing-image-keyword-spy/raw/secret/start2.sh --no-check-certificate --directory-prefix=/home/keyword
+wget https://github.com/nurd1n/bing-image-keyword-spy/raw/secret/start3.sh --no-check-certificate --directory-prefix=/home/keyword
+wget https://github.com/nurd1n/bing-image-keyword-spy/raw/secret/split.sh --no-check-certificate --directory-prefix=/home/keyword
 wget https://github.com/nurd1n/bing-image-keyword-spy/raw/secret/title.py --no-check-certificate --directory-prefix=/home/keyword
 chmod 755 *.sh
