dabe1acc9245680d661ce15829d52fa1b2e21910
Nurdianto <nurdiantopntr@gmail.com>
2016-04-22 08:26:18 +0700

    Update start2.sh

--- start2.sh
+++ start2.sh
@@ -1,4 +1,4 @@
 IFS=';'
 while read f1; do
-    echo "cat title.py | sed -e 's|bingurl|$f1|g' -e 's|bingurl|\&|g' | python - >> deletekeywordbing.txt" | bash -
+    echo "cat title.py | sed -e 's|bingurl|$f1|g' -e 's|bingurl|\&|g' | python - | tr A-Z a-z >> deletekeywordbing.txt" | bash -
 done < deleteurl.txt
